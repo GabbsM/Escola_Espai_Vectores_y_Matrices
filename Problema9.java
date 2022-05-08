a83edd06c823bc7ac02325298b3ce9dd7c282765
GabbsM <gabbsmoreno.gm@gmail.com>
2022-05-08 12:53:48 +0200

    Bloque 2. Vectores y Matrices.
    Agregamos ejercicio 9.

--- Problema9.java
+++ Problema9.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Problema9 {
     //Problema 9
     //Un polinomio de grado N se puede representar como una tabla unidimensional (un vector) de
@@ -9,6 +11,24 @@ public class Problema9 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Grado de polinomio: ");
+        int gradoPolinomio = sc.nextInt();
 
+        int array [] = new int[gradoPolinomio+1];
+        for (int i = 0; i < gradoPolinomio + 1  ; i++) {
+            System.out.println("Coeficiente del numero " +i + " : ");
+            array[i] = sc.nextInt();
+
+        }
+
+        System.out.println("Introduce el valor de a: ");
+        int a = sc.nextInt();
+        int Pa= 0;
+
+        for (int i = 0; i < gradoPolinomio +1  ; i++) {
+            Pa = (int) (Pa + Math.pow(a,i)*array[i]);
+        }
+        System.out.println("Valor polinomio en X = " + a + " es: "+ Pa);
     }
 }
